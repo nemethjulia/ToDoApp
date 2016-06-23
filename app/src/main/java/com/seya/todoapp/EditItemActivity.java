e474969208e6a35e4398d213b289af8a01baaec6
Julia Szele-Nemeth <julia.szelenemeth@gmail.com>
2016-06-23 00:39:31 -0700

    Edit function - Close the Edit view on Save, send back the new data

--- app/src/main/java/com/seya/todoapp/EditItemActivity.java
+++ app/src/main/java/com/seya/todoapp/EditItemActivity.java
@@ -1,7 +1,9 @@
 package com.seya.todoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class EditItemActivity extends AppCompatActivity {
@@ -15,5 +17,13 @@ public class EditItemActivity extends AppCompatActivity {
         String itemText = getIntent().getStringExtra("item");
         etEditItem = (EditText) findViewById(R.id.etEditItem);
         etEditItem.setText(itemText);
+    }
+
+    public void onSave(View view) {
+        Intent data = new Intent();
+        data.putExtra("item", etEditItem.getText().toString());
+
+        setResult(RESULT_OK, data);
+        this.finish();
     }
 }
