667086836f6b8aead9dead80e45a250339d4614a
Julia Szele-Nemeth <julia.szelenemeth@gmail.com>
2016-06-23 00:15:37 -0700

    Edit function - Edit view with initial value

--- app/src/main/java/com/seya/todoapp/EditItemActivity.java
+++ app/src/main/java/com/seya/todoapp/EditItemActivity.java
@@ -2,12 +2,18 @@ package com.seya.todoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditItemActivity extends AppCompatActivity {
+
+    private EditText etEditItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_item);
+        String itemText = getIntent().getStringExtra("item");
+        etEditItem = (EditText) findViewById(R.id.etEditItem);
+        etEditItem.setText(itemText);
     }
 }
